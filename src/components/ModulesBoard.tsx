39d06064612dfa1e993501d25e326f8807c60243
grisley <jorge.cobreros@protonmail.com>
2024-12-30 17:05:32 +0100

    input modal

--- src/components/ModulesBoard.tsx
+++ src/components/ModulesBoard.tsx
@@ -2,8 +2,10 @@
 import { Module } from "../types";
 import Card from "./Card";
 import AddIcon from '@mui/icons-material/Add';
+import ModuleInputModal from "./ModuleInputModal";
+import { useState } from "react";
 export default function ModulesBoard({modules, modulesRef, button}: {modules: Module[], modulesRef: React.RefObject<HTMLUListElement>, button?: boolean}) {
-
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="card-container">
@@ -14,12 +16,13 @@ export default function ModulesBoard({modules, modulesRef, button}: {modules: Mo
           </Card>
         ))}
         {button && 
-          <Card id="no-drag" key="no-drag" onClick={() => console.log("click")}>
+          <Card id="no-drag" key="no-drag" onClick={() => setOpen(!open)}>
             <AddIcon  sx={{height: '80px', width: '80px' }} />
           </Card>
         }
       </ul>
- 
+      <ModuleInputModal open={open} setOpen={setOpen} />
+
     </div>
     
   );

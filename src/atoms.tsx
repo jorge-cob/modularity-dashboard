1028e8728eeaa59054446fe06c48f68af7fef5f1
grisley <jorge.cobreros@protonmail.com>
2025-01-02 12:48:45 +0100

    persistent state

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,5 +1,8 @@
-import { atom } from 'jotai';
 import { Module } from "./types";
+import { atomWithStorage, createJSONStorage } from 'jotai/utils'
 
-export const availableModulesAtom = atom<Module[]>([]);
-export const activeModulesAtom = atom<Module[]>([]);
+
+const storage = createJSONStorage<Module[]>(() => sessionStorage);
+
+export const availableModulesAtom = atomWithStorage<Module[]>('availableModules', [], storage)
+export const activeModulesAtom = atomWithStorage<Module[]>('activeModules', [], storage)
